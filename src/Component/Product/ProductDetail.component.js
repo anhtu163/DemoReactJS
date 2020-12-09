536e8d7c493c2fc88c2e8ae80179cb8168ef64c9
vuvvo <vuvvo@kms-technology.com>
2020-12-09 10:07:50 +0700

    add cart

--- src/Component/Product/ProductDetail.component.js
+++ src/Component/Product/ProductDetail.component.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import '../../Style/Container.style.css';
+import productImg from "../../Image/product.PNG";
+import '../../Style/DetailComponent.css'
+import Button from "@material-ui/core/Button";
+import {TextField} from "@material-ui/core";
+
 
 export default function ProductDetail (props){
     //console.log(props.match.param.id);
     return(
         <div className="container">
             {/*//{props.match.param.id}*/}
+            <div className="detail-wrapper">
+                <img className="detail-image" src={productImg} alt="product"/>
+                <div className="detail-info">
+                    <h2>9-layer medical mask</h2>
+                    <h3>$950.00</h3>
+                    <TextField
+                        id="outlined-number"
+                        label="Number"
+                        type="number"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        variant="outlined"
+                    />
+                    <Button style={{color: 'white', backgroundColor: 'orangered',height:'56px'}} >Add to Cart</Button>
+                </div>
+            </div>
+            <div className="detail-description">
+                <h4>About this product</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ligula mauris, auctor ut
+                    sollicitudin sed, ullamcorper at purus. Etiam quis lacinia orci. Sed accumsan tempus enim. Nam
+                    commodo mauris ac neque fermentum, non bibendum nisi sollicitudin. In fermentum ipsum sed nibh
+                    egestas, ut eleifend dui condimentum. Integer ut sapien velit. Curabitur commodo libero eu est
+                    commodo, ac ornare nibh suscipit. Nullam tincidunt nec mi a ultricies.</p>
+            </div>
         </div>
     )
 }
--- src/Router/Root.js
+++ src/Router/Root.js
@@ -1,29 +1,23 @@
 import {Route, BrowserRouter , Switch} from 'react-router-dom';
-import SignInContainer from "../Container/SignIn.container";
-import HomeContainer from "../Container/Home.container";
+import CartContainer from "../Container/Cart.container";
 import ProductDetailContainer from "../Container/ProductDetail.container";
-import NavMenu from "../Component/NavMenu.component";
+import HomeContainer from "../Container/Home.container";
 
 const Root = () => (
-    <div>
-        <NavMenu />
-        <BrowserRouter>
-            <Switch>
-                <Route  path="/signin">
-                    <SignInContainer />
-                </Route>
-                <Route  path="/product-detail&id=:id">
-                    <ProductDetailContainer />
-                </Route>
-                <Route  path="/">
-                    <HomeContainer />
-                </Route>
+    <BrowserRouter>
+        <Switch>
 
-
-            </Switch>
-        </BrowserRouter>
-    </div>
-
+            <Route  path="/detail">
+                <ProductDetailContainer />
+            </Route>
+            <Route  path="/cart">
+                <CartContainer />
+            </Route>
+            <Route  path="/">
+                <HomeContainer />
+            </Route>
+        </Switch>
+    </BrowserRouter>
 )
 
 
