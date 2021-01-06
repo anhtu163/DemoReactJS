74ed84393c4955f259a3bc7528d677cf8a8e4ae6
vuvvo <vuvvo@kms-technology.com>
2021-01-06 16:30:40 +0700

    fix huge bug

--- src/Container/ProductCard.container.tsx
+++ src/Container/ProductCard.container.tsx
@@ -1,8 +1,6 @@
 import {connect, ConnectedProps} from "react-redux"
 import ProductCard from "../Component/Home/ProductCard.component";
-import {getDetailProductRequest} from "../Action/Product.action";
 import {compose} from "redux";
-import Home from "../Component/Home/Home.component";
 import {ThunkDispatch} from "redux-thunk";
 import {IRootState} from "../Reducer/Reducer";
 
@@ -13,7 +11,6 @@ const mapStateToProps = (state : IRootState) =>{
 }
 const mapDispatchToProps = (dispatch: ThunkDispatch<IRootState, any, any>) => {
     return {
-        getAllProduct : () => {dispatch(getDetailProductRequest())},
     }
 }
 
